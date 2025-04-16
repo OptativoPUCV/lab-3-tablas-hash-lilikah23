5825180a13fd8a5a61156a538efeefe0f86352c2
Your Name <you@example.com>
2025-04-16 09:13:37 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -40,7 +40,18 @@ int is_equal(void* key1, void* key2){
 
 
 void insertMap(HashMap * map, char * key, void * value) {
-
+    long indice = hash(key, map->capacity);
+    long original = indice;
+    while(map->buckets[indice] != NULL && map->buckets[indice]->key != NULL)
+    {
+        if(is_equal(map->buckets[indice]->key,key)) return;
+        indice = (indice + 1) % map->capacity;
+        if(indice == original)return;
+    }
+    map->buckets[indice]= createPair(key,value);
+    map->size++;
+    map->current = indice;
+    
 
 }
 
